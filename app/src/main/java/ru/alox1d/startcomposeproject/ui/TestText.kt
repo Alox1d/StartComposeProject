9ac1bcb61d9ea465e210e0e11d84d427473a4293
Alox1d <alox1dab@gmail.com>
2024-02-03 20:24:56 +0300

    added fun ColumnScope.TestText(count: Int, text: String)

---
+++ app/src/main/java/ru/alox1d/startcomposeproject/ui/TestText.kt
@@ -0,0 +1,27 @@
+package ru.alox1d.startcomposeproject.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ColumnScope.TestText(count: Int, text: String) {
+    repeat(count) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = text
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewText() {
+    Column(Modifier.fillMaxSize()) {
+        TestText(text = "Text", count = 5)
+    }
+}
